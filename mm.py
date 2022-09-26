a47ad6111eeec834fe943587dd7430dc0f09f543
M254-bto <ngechamike26@gmail.com>
2022-09-26 18:15:23 +0300

    sklearn

--- mm.py
+++ mm.py
@@ -1,21 +1,21 @@
-from sklearn.datasets import load_iris
-import joblib
-from PIL import Image
-# import sklearn
+# from sklearn.datasets import load_iris
+# import joblib
+# from PIL import Image
+# # import sklearn
 
-# print(sklearn.__version__)
+# # print(sklearn.__version__)
 
-model = open('iris_Classifier.pkl', 'rb')
-model_ = joblib.load(model)
+# model = open('iris_Classifier.pkl', 'rb')
+# model_ = joblib.load(model)
 
 
-def params_list():
-    return load_iris().feature_names
+# def params_list():
+#     return load_iris().feature_names
 
-def images():
-    setosa= Image.open('setosa.png')
-    versicolor= Image.open('versicolor.png')
-    virginica = Image.open('virginica.png')
-    list_ = [setosa, versicolor, virginica]
-    return list_
+# def images():
+#     setosa= Image.open('setosa.png')
+#     versicolor= Image.open('versicolor.png')
+#     virginica = Image.open('virginica.png')
+#     list_ = [setosa, versicolor, virginica]
+#     return list_
 
--- str.py
+++ str.py
@@ -1,7 +1,27 @@
 import streamlit as st
-from mm import params_list, model_, images
 import pandas as pd
-import sklearn
+from sklearn.datasets import load_iris
+import joblib
+from PIL import Image
+# import sklearn
+
+# print(sklearn.__version__)
+
+model = open('iris_Classifier.pkl', 'rb')
+model_ = joblib.load(model)
+
+
+def params_list():
+    return load_iris().feature_names
+
+def images():
+    setosa= Image.open('setosa.png')
+    versicolor= Image.open('versicolor.png')
+    virginica = Image.open('virginica.png')
+    list_ = [setosa, versicolor, virginica]
+    return list_
+
+
 
 
 
